5f29ad9f31ac617837a17e645fdab361b45cbc96
ZeusenAres <2060537@talnet.nl>
2022-05-01 12:22:47 +0200

    completed login game
    completed login game

--- PlatformHub/PlatformHub/Classes/BackendRegister.class.php
+++ PlatformHub/PlatformHub/Classes/BackendRegister.class.php
@@ -7,9 +7,6 @@ class BackendRegister extends Database
 
     public function CreateUser(string $user, string $password, string $repeatedPassword)
     {
-        $this->ValidateUser($user);
-        $this->ValidatePassword($password);
-        $this->ValidateRepeatedPassword($password, $repeatedPassword);
         $this->CheckIfUserExists($user);
 
         $statement = $this->connection->prepare("INSERT INTO $this->table (username, password) values (:username, :password)");
@@ -19,37 +16,17 @@ class BackendRegister extends Database
         ]);
     }
 
-    private function ValidateUser(string $user)
-    {
-        if (strlen(trim($user)) == 0)
-        {
-            echo '[Geef een usernaam op]' . PHP_EOL;
-        }
-    }
-
-    private function ValidatePassword(string $password)
-    {
-        if (strlen(trim($password)) == 0)
-        {
-            echo '[Geef een wachtwoord op]' . PHP_EOL;
-        }
-    }
-
-    private function ValidateRepeatedPassword(string $password, string $repeatedPassword)
-    {
-        if (trim($password) != trim($repeatedPassword))
-        {
-            echo '[Wachtwoorden moeten hetzelfde zijn]' . PHP_EOL;
-        }
-    }
-
     private function CheckIfUserExists(string $user)
     {
         $statement = $this->connection->prepare("select username from $this->table where username = :username");
         $statement->execute([":username" => $user]);
-        if ($statement->fetch() == 1)
+        if ($statement->fetch())
         {
-            echo "User $user already exists!" . PHP_EOL;
+            echo "User already exists" . PHP_EOL;
+        }
+        else
+        {
+            echo 'created';
         }
     }
 }
--- PlatformHub/PlatformHub/registerbackend.php
+++ PlatformHub/PlatformHub/registerbackend.php
@@ -4,20 +4,35 @@ require_once 'Classes/BackendRegister.class.php';
 $db = new BackendRegister();
 $db->DBConnect();
 
-//$username = $_POST['username'];
-//$password = $_POST['password'];
-//$repeatedPassword = $_POST['repeatedpassword'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$repeatedPassword = $_POST['repeatedpassword'];
 
-$username = 'abse';
-$password = 'd';
-$repeatedPassword = 'd';
+//$username = 'abse';
+//$password = 'd';
+//$repeatedPassword = 'd';
 
-if($username != null && $password != null && $repeatedPassword != null)
+if($username == null)
+{
+    echo "Username is required" . PHP_EOL;
+}
+
+if($password == null)
+{
+    echo "Password is required" . PHP_EOL;
+}
+
+if($password != $repeatedPassword)
+{
+    echo "Passwords must match" . PHP_EOL;
+}
+
+if($username != null && $password && $repeatedPassword != null)
 {
     $db->CreateUser($username, $password, $repeatedPassword);
-    echo 'created';
 }
-else{
-    
+else
+{
+    echo 'Please fill in the required fields' . PHP_EOL;
 }
 ?>
